16110318d46458573bb71d531692a9a78c6fbc97
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-06-28 01:00:34 +0530

    Update Basic_Recursion_problem.c

--- Basic_Recursion_problem.c
+++ Basic_Recursion_problem.c
@@ -1,3 +1,5 @@
+//To find the no of digits in a given number
+
 
 #include <stdio.h>
 int no_of_digits(int);
@@ -6,7 +8,6 @@ int main()
     int n;
     scanf("%d",&n);
     printf("%d",no_of_digits(n));
-
     return 0;
 }
 int no_of_digits(int n){
@@ -14,3 +15,57 @@ int no_of_digits(int n){
     return 0;
     return no_of_digits(n/10)+1;
 }
+
+//Accurate method
+#include <stdio.h>
+#include <math.h>
+void main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",(int)floor(log10(n)+1));
+}
+
+
+//To find the sum of digits of given no
+
+
+int sum_of_digits(int);
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",sum_of_digits(n));
+
+    return 0;
+}
+int sum_of_digits(int n){
+    if(n==0)
+    return 0;
+    return sum_of_digits(n/10)+n%10;
+}
+
+
+//To find the given nuumber is armstrong
+
+
+#include <stdio.h>
+#include <math.h>
+int digits(int);
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a=digits(n);
+    if(a==n)
+    printf("Armstrong");
+    else
+    printf("Not an Armstrong");
+    return 0;
+}
+int digits(int n){
+    if(n==0)
+    return 0;
+    return digits(n/10)+pow(n%10,3);
+}
+
+////To find the given nuumber is Pallindrom
