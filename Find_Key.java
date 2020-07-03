98422c153ecca81ccfd8586f3e53fbee560d374a
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-07-03 17:16:24 +0530

    Update Find_Key.java

--- Find_Key.java
+++ Find_Key.java
@@ -66,4 +66,49 @@ public class Main
 //Key=[Largetst of all the thousands places digits of all 3 numbers-Smallest of all the thousands places digits of all 3 numbers][Largetst of all the hundred places digits of all 3 numbers-Smallest of all the hundred places digits of all 3 numbers][Largetst of all the tenth places digits of all 3 numbers-Smallest of all the tenth places digits of all 3 numbers][Largetst of all the ones places digits of all 3 numbers-Smallest of all the ones places digits of all 3 numbers] e.g input1 3621. input2-2452, input3-1352. then Key is [3-1][5-3][5-2][2-1]=2231
 
 
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int m,n,s=0,w=0,input1=sc.nextInt(),input2=sc.nextInt(),input3=sc.nextInt(),i=0;
+	    int[] gh=find(input1);
+	    s=s+gh[3];
+	    w=w+gh[0];
+	    gh=find(input2);
+	    s=s+gh[3];
+	    w=w+gh[0];
+	    gh=find(input3);
+	    s=s+gh[3];
+	    w=w+gh[0];
+	    System.out.println(s-w);
+	}
+	    public static int[] find(int n){
+	         int a[]=new int[4];
+	         int m,i=0;
+	         while(n!=0){
+	             a[i++]=n%10;
+	             n/=10;
+	         }
+	         for(int k=0;k<4;k++){
+	             for(int j=0;j<4-k-1;j++){
+	                 if(a[j]>a[j+1]){
+	                     m=a[j];
+	                     a[j]=a[j+1];
+	                     a[j+1]=m;
+	                 }
+	             }
+	         }
+	         return a;
+	    }
+}
+
+
+
+//1000<<input1<<9999
+//1000<<input2<<99991000 and1000 <<input3<<9999 You are expected to find the Key using the below formula
+//Key=sum of the largest digits of all 3-sum of the smallest digits of all 3 e.g input1 3621. input2-2452, input3-1352. then Key is (5+5+5)-(1+2+1)=11
+
+
                                                                                                                    
